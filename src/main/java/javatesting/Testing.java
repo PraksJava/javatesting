6c03ff3bf1d0771d15d7d4dd7e76e571efed9587
PraksJava <33279504+PraksJava@users.noreply.github.com>
2022-08-23 19:04:05 +0530

    Add files via upload

---
+++ src/main/java/javatesting/Testing.java
@@ -0,0 +1,15 @@
+package javatesting;
+
+public class Testing {
+	
+	public static void main(String[] args) {
+		System.out.println("Tesing");
+		mavenTest();
+	}
+
+	private static void mavenTest() {
+		String record = "test";
+		
+	}
+
+}
